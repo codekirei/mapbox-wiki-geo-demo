bde8a0f87e295d7b0a7537d1880ef8ecbb10b2c3
Jacob Blakely <jacob@codekirei.com>
2016-01-19 20:42:11 -0800

    add and move marker on map

--- src/scripts/app/map.js
+++ src/scripts/app/map.js
@@ -4,5 +4,29 @@ import 'mapbox.js'
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiY29kZWtpcmVpIiwiYSI6ImNpamVxdnl5azAwMDB0dW01amxueGJkZHQifQ.pCGbQleCKfjcAagSQSFjSw'
 
+const zoom = 14
+const coordinates = [[44.56, -123.28], [35.68, 139.68]]
+
 const map = L.mapbox.map('map', 'mapbox.streets')
-  .setView([40, -74.50], 9)
+  .setView(coordinates[0], zoom)
+
+const marker = L.marker(coordinates[0]).addTo(map)
+
+const popup = L.popup(
+  { closeButton: false
+  })
+  .setContent('<span>Hello world!</span>')
+
+marker.bindPopup(popup).openPopup()
+
+// artificial loop
+const t = 3000
+const len = coordinates.length
+let i = 0
+const loop = () => setTimeout(() => {
+  i = (i +1) % len
+  marker.setLatLng(coordinates[i])
+  map.setView(coordinates[i], zoom)
+  loop()
+}, t)
+// loop()
--- todo.md
+++ todo.md
@@ -1,5 +1,8 @@
-- get mapbox working inside mithril
+- get mapbox working inside mithril?
 - clean up and consolidate styles
+
+- add info button and overlay
+- add styled notification to users with JS disabled
 
 - set up node process to run on server and update all connected clients when there are changes
   - should only ping when there are clients connected
